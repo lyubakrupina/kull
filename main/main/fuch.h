b8dddd4199b46009588609b57771005e293cce5b
Krupina Lyuba <lyubakrupina@yandex.ru>
2019-11-26 16:31:36 +0300

    add function Sum

---
+++ main/main/fuch.h
@@ -0,0 +1,7 @@
+// заголовочный файл func.h
+#ifndef __FUNC_H__ 
+#define __FUNC_H__
+
+double MySum(double, double);
+
+#endif // __FUNC_H__
---
+++ main/main/func.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "fuch.h"
+#include <cmath>
+
+double MySum(double a, double b)
+{
+	return a + b;
+}
